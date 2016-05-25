a347b9e60cc12f59ca939fe0784c6fb59caa88a8
Tomas Kulhanek <tomaton@centrum.cz>
2016-05-25 09:03:49 +0100

    minor changes

--- wp6-virtualfolder/bootstrapccp4.sh
+++ wp6-virtualfolder/bootstrapccp4.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # download ccp4, configure and install
+echo Downloading and installing CCP4, it may take several minutes ...
 wget -q ftp://ftp.ccp4.ac.uk/ccp4/current/ccp4-7.0-linux-x86_64.tar.bz2
 tar -xjf ccp4-7.0-linux-x86_64.tar.bz2
 mv ccp4-7.0 ccp4
