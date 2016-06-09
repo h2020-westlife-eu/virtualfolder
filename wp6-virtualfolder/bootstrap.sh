4cc6fd0979d5939228e0f38093f2b9f55860055c
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-06-09 15:29:20 +0100

    added DLL as workaround to build .NET in Linux using MONO

--- wp6-virtualfolder/bootstrap.sh
+++ wp6-virtualfolder/bootstrap.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-#02.06.2016 tomas- added fromdos to all scripts as vagrant fixes CR-LF ending only on bootstrap.sh and not other scripts.
+#02.06.2016 tomas - added fromdos to all scripts as vagrant fixes CR-LF ending only on bootstrap.sh and not other scripts.
+#09.06.2016 tomas - workaround, stopped ccp4 installation, not appropriate licence for further distribution
 sudo apt-get install -y tofrodos
 fromdos /vagrant/*.sh
 fromdos /vagrant/rc.local
 /vagrant/bootstrapweb.sh
 /vagrant/bootstrapconf.sh
-/vagrant/bootstrapccp4.sh
+#/vagrant/bootstrapccp4.sh
 /vagrant/bootstrapservice.sh
 /vagrant/bootstrapstart.sh
 
