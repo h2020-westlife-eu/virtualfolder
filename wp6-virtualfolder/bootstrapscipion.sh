7d9435be9ec02127e1652388710210e1cc7ecbba
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-07-11 09:18:15 +0100

    minor update

--- wp6-virtualfolder/bootstrapscipion.sh
+++ wp6-virtualfolder/bootstrapscipion.sh
@@ -13,7 +13,7 @@ update-java-alternatives -s java-8-oracle
 apt-get install -y -qq oracle-java8-set-default
 
 #install scipion
-
+echo downloading SCIPION
 wget -q http://scipion.cnb.csic.es/m/static/install/scipion_v1.0.1_2016-06-30_linux64.tgz -O /home/vagrant/scipion.tgz
 tar -xzf /home/vagrant/scipion.tgz -C /home/vagrant
 cd /home/vagrant/scipion/
