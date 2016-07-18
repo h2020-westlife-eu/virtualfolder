cc17de9eb8ef095b76634c5aa480c4012d68cbd9
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-07-18 11:42:00 +0100

    localhost as a VM desktop background
    Former-commit-id: 8713bfe090db484e5b570794a805cc0a792e4a66

--- wp6-virtualfolder/bootstrapscipion.sh
+++ wp6-virtualfolder/bootstrapscipion.sh
@@ -17,10 +17,7 @@ echo downloading SCIPION
 wget -q http://scipion.cnb.csic.es/m/static/install/scipion_v1.0.1_2016-06-30_linux64.tgz -O /home/vagrant/scipion.tgz
 tar -xzf /home/vagrant/scipion.tgz -C /home/vagrant
 cd /home/vagrant/scipion/
-export MPI_LIBDIR=/usr/lib/openmpi/lib
-export MPI_INCLUDE=/usr/lib/openmpi/include
-export MPI_BINDIR=/usr/bin
 
-./scipion config
 #change owner, otherwise tests will fail for permission denied
 chown -R vagrant:vagrant /home/vagrant/scipion
+sudo -i -u vagrant export MPI_LIBDIR=/usr/lib/openmpi/lib;export MPI_INCLUDE=/usr/lib/openmpi/include;export MPI_BINDIR=/usr/bin;/home/vagrant/scipion/scipion config
