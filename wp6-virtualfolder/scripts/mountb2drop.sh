54edeafb24c6939e38b41582ecd563b656d1e5de
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-08-03 16:12:11 +0100

    default xfce desktop, updated scripts and default web page

--- wp6-virtualfolder/scripts/mountb2drop.sh
+++ wp6-virtualfolder/scripts/mountb2drop.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # 24.05.2016 tomas - changed directory structure, all mounts will be subdir of 'work', comment owncloudcmd
 whoami 1>&2
-#create directory if not created 
+#create directory if not created
 mkdir -p /home/vagrant/work/b2drop
 chown www-data /home/vagrant/work
 #mount work folder via webdav to b2drop
 umount /home/vagrant/work/b2drop
 #kill all previous b2dropsync and xiacont
-killall /bin/sh
+#killall /bin/sh
 mv /tmp/secrets /etc/davfs2/secrets
 chown root:root /etc/davfs2/secrets
 chmod 600 /etc/davfs2/secrets
@@ -17,4 +17,3 @@ mount.davfs https://b2drop.eudat.eu/remote.php/webdav /home/vagrant/work/b2drop
 #start owncloud synchronization of local copy
 #sudo -u vagrant nohup /home/vagrant/scripts/b2dropsync.sh > /home/vagrant/logs/b2drop.log 2>&1 &
 #sudo -u vagrant nohup /home/vagrant/scripts/xiacont.sh > /home/vagrant/logs/xia.log 2>&1 &
-
