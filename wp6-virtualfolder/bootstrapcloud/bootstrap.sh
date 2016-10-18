797c9f2e014b8fadf5f8b96c74b5e3f4935c899f
TomasKulhanek <tomaton@centrum.cz>
2016-10-18 16:10:02 +0100

    ADD: cloud bootstrap

---
+++ wp6-virtualfolder/bootstrapcloud/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+/home/vagrant/bootstrap/bootstrapweb.sh
+/home/vagrant/bootstrap/bootstrapservice.sh
+/home/vagrant/bootstrap/bootstrapscipion.sh
+/home/vagrant/bootstrap/bootstrapvirtuoso.sh
+/home/vagrant/bootstrap/bootstrapstart.sh
