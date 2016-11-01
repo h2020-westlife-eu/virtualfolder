1a3ec1fef97e5e4e1795e3b795acb42e6033b0d5
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-01 15:19:26 +0000

    minor fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,19 +5,27 @@
 # get a wp6 master (3.3 MB) or git clone (21 MB) #
 ##################################################
 
-echo downloading west-life-wp6
-wget --quiet https://github.com/h2020-westlife-eu/west-life-wp6/archive/master.zip
-unzip -q master.zip -d /home/vagrant
-rm master.zip
+# echo downloading west-life-wp6
+# wget --quiet https://github.com/h2020-westlife-eu/west-life-wp6/archive/master.zip
+# unzip -q master.zip -d /home/vagrant
+# rm master.zip
+
 # or alternatively git clone, commented
-#git clone https://github.com/h2020-westlife-eu/west-life-wp6.git
-#mv west-life-wp6 west-life-wp6-master
+# git clone https://github.com/h2020-westlife-eu/west-life-wp6.git
+# mv west-life-wp6 west-life-wp6-master
+#
+# export WP6SRC=/home/vagrant/west-life-wp6-master/wp6-virtualfolder
+
+#################################################
+# or use wp6 sources from vagrant shared folder #
+#################################################
+
+export WP6SRC=/home/vagrant/work/local/wp6-virtualfolder
 
 ###########################
 # launch bootstrap script #
 ###########################
 
-export WP6SRC=/home/vagrant/west-life-wp6-master/wp6-virtualfolder
 mkdir -p /home/vagrant/bootstrap
 cp -R $WP6SRC/bootstrap/* /home/vagrant/bootstrap
 dos2unix /home/vagrant/bootstrap/*
