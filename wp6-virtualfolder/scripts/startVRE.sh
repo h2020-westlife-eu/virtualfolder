15ba2a352a3937fc71174aca3cf0a48beec5902b
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-15 19:58:17 +0100

    UPDATED bash

--- wp6-virtualfolder/scripts/startVRE.sh
+++ wp6-virtualfolder/scripts/startVRE.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 nohup /home/vagrant/scripts/runVRE.sh > /home/vagrant/logs/VRE.log 2>&1 &
--- wp6-virtualfolder/scripts/startVirtuoso.sh
+++ wp6-virtualfolder/scripts/startVirtuoso.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 nohup /cvmfs/west-life.egi.eu/software/virtuoso/latest/bin/virtuoso-t -f -c /home/vagrant/bootstrap/virtuoso.ini > /home/vagrant/logs/virtuoso.log 2>&1 &
