e8be7e5a41510683141c5c2dc7e966115b240b5e
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-15 19:59:29 +0100

    UPDATED version 7

--- wp6-virtualfolder/scripts/startCCP4.sh
+++ wp6-virtualfolder/scripts/startCCP4.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #/cvmfs/facilities.gridpp.ac.uk/ccp4/devel/Version5/ccp4-7.0/BINARY.setup
-source /cvmfs/facilities.gridpp.ac.uk/ccp4/devel/Version5/ccp4-7.0/bin/ccp4.setup-sh
-/cvmfs/facilities.gridpp.ac.uk/ccp4/devel/Version5/ccp4-7.0/start
+source /cvmfs/facilities.gridpp.ac.uk/ccp4/devel/Version7/ccp4-7.0/bin/ccp4.setup-sh
+/cvmfs/facilities.gridpp.ac.uk/ccp4/devel/Version7/ccp4-7.0/start
