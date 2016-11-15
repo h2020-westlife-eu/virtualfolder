eddfcfeefedca885d0b39eeb3a789daa7ffdc147
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-15 19:52:59 +0100

    UPDATED scipion web start

--- wp6-virtualfolder/scripts/startScipion.sh
+++ wp6-virtualfolder/scripts/startScipion.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# copy hosts.conf to .config
+mkdir -p /home/vagrant/.config/scipion/myfirstmap
+mkdir -p /home/vagrant/.config/scipion/mymovies
+mkdir -p /home/vagrant/.config/scipion/myresmap
+cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/config/hosts.conf /home/vagrant/.config/scipion/myfirstmap
+cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/config/hosts.conf /home/vagrant/.config/scipion/mymovies
+cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion/config/hosts.conf /home/vagrant/.config/scipion/myresmap
+
 /cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion config
-/cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion
+/cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion webserver
 
