50b2cef2d89a8afe7895d864b53c1c94f1ecc3ae
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-15 19:54:22 +0100

    ADD scipion web UPDATED scipion script

--- wp6-virtualfolder/scripts/startScipion.sh
+++ wp6-virtualfolder/scripts/startScipion.sh
@@ -3,10 +3,10 @@
 mkdir -p /home/vagrant/.config/scipion/myfirstmap
 mkdir -p /home/vagrant/.config/scipion/mymovies
 mkdir -p /home/vagrant/.config/scipion/myresmap
-cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/config/hosts.conf /home/vagrant/.config/scipion/myfirstmap
-cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/config/hosts.conf /home/vagrant/.config/scipion/mymovies
-cp /cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion/config/hosts.conf /home/vagrant/.config/scipion/myresmap
+cp /cvmfs/west-life.egi.eu/software/scipion/latest/config/hosts.conf /home/vagrant/.config/scipion/myfirstmap
+cp /cvmfs/west-life.egi.eu/software/scipion/latest/config/hosts.conf /home/vagrant/.config/scipion/mymovies
+cp /cvmfs/west-life.egi.eu/software/scipion/latest/scipion/config/hosts.conf /home/vagrant/.config/scipion/myresmap
 
-/cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion config
-/cvmfs/west-life.egi.eu/software/scipion/latestweb/scipion webserver
+/cvmfs/west-life.egi.eu/software/scipion/latest/scipion config
+/cvmfs/west-life.egi.eu/software/scipion/latest/scipion webserver
 
