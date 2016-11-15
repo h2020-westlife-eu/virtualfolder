5ca7064b3657375374f748da7dd032eb14043ab2
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-15 19:57:09 +0100

    UPDATED sleep

--- wp6-virtualfolder/scripts/startMetaDataService.sh
+++ wp6-virtualfolder/scripts/startMetaDataService.sh
@@ -1,2 +1,6 @@
-source /cvmfs/west-life.egi.eu/tools/mono/mono-dev-env
-nohup mono /home/vagrant/build/MetadataService/MetadataService.exe > /home/vagrant/logs/MetadataService.log 2>&1 &
+#!/usr/bin/env bash
+sleep 15
+cd /cvmfs/west-life.egi.eu/tools/mono/
+cd
+source /cvmfs/west-life.egi.eu/tools/mono/mono-dev-env > /home/vagrant/logs/MetadataService.log 2>&1 &
+nohup mono /home/vagrant/build/MetadataService/MetadataService.exe >> /home/vagrant/logs/MetadataService.log 2>&1 &
