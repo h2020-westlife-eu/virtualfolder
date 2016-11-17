29ac4af73f58e768f71bd37af736657698d0b126
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2016-11-17 09:42:49 +0000

    FIX: source

--- wp6-virtualfolder/scripts/startMetaDataService.sh
+++ wp6-virtualfolder/scripts/startMetaDataService.sh
@@ -2,5 +2,5 @@
 sleep 15
 cd /cvmfs/west-life.egi.eu/tools/mono/
 cd
-source /cvmfs/west-life.egi.eu/tools/mono/mono-dev-env > /home/vagrant/logs/MetadataService.log 2>&1 &
+source /cvmfs/west-life.egi.eu/tools/mono/mono-dev-env > /home/vagrant/logs/MetadataService.log 2>&1
 nohup mono /home/vagrant/build/MetadataService/MetadataService.exe >> /home/vagrant/logs/MetadataService.log 2>&1 &
