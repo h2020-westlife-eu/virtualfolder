9471bbb0a954d90e765b157492ee476bd3d29579
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-02-17 11:39:29 +0000

    UPDATE: check ssl certificate

--- wp6-virtualfolder/src/WP6Service2/WP6Service2/Services/Files/DropboxProvider.cs
+++ wp6-virtualfolder/src/WP6Service2/WP6Service2/Services/Files/DropboxProvider.cs
@@ -5,6 +5,8 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Dropbox.Api;
 using MetadataService.Services.Settings;
@@ -47,6 +49,26 @@ namespace MetadataService.Services.Files
             return ListOfFiles(path);
         }
 
+        public bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+            bool isOk = true;
+            // If there are errors in the certificate chain, look at each error to determine the cause.
+            if (sslPolicyErrors != SslPolicyErrors.None) {
+                for (int i=0; i<chain.ChainStatus.Length; i++) {
+                    if (chain.ChainStatus [i].Status != X509ChainStatusFlags.RevocationStatusUnknown) {
+                        chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+                        chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+                        chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+                        chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+                        bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+                        if (!chainIsValid) {
+                            isOk = false;
+                        }
+                    }
+                }
+            }
+            return isOk;
+        }
+
 
         public async Task Initialize(){
             //TODO change access token to user specific
@@ -55,6 +77,7 @@ namespace MetadataService.Services.Files
                 if (accesstoken.Length==0) return;
                 //setting proxy if it is defined in environment
                 var environmentVariable = System.Environment.GetEnvironmentVariable("http_proxy");
+                ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
                 if (!string.IsNullOrEmpty(environmentVariable))
                 {
                     Console.WriteLine("Setting proxy for dropboxclient:" +
