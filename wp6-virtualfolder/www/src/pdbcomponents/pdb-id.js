409c27b0497533c72d999e5a32686c24ed8ba5e6
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-02-28 14:32:10 +0000

    UPDATE: added name, submit button, remove item to dataset demo feature

--- wp6-virtualfolder/www/src/pdbcomponents/pdb-id.js
+++ wp6-virtualfolder/www/src/pdbcomponents/pdb-id.js
@@ -2,6 +2,8 @@
  * Created by Tomas Kulhanek on 2/27/17.
  */
 
+// PdbId => pdb-id MyCustomAttribute => my-custom-attribute
+
 export class PdbIdCustomAttribute {
   static inject = [Element]
 
@@ -12,12 +14,18 @@ export class PdbIdCustomAttribute {
   /* set's DOM atribute to the binded value.
    valueChanged() is triggered by Aurelia,
    when the value is bind - before attached()
+   this.value
    */
   valueChanged(newValue,oldValue){
    //console.log('valueChanged() element:'+this.element.tagName+" newvalue:"+newValue+' this.value:'+this.value+' current.attribute.pdb-id:'+ this.element.getAttribute('pdb-id'));
+    // <pdb-redo pdb-id="item"></pdb-redo> -> valueChange()
    this.element.setAttribute('pdb-id',this.value);
+   //  <pdb-redo pdb-id="2sc3">
+
    //console.log(this.element.getAttribute('pdb-id'));
   }
+
+
 
   /* bootstrap the pdb component library just from the element */
   attached(){
--- wp6-virtualfolder/www/src/pdbcomponents/pdb-ids.js
+++ wp6-virtualfolder/www/src/pdbcomponents/pdb-ids.js
@@ -4,8 +4,9 @@
 
 export class PdbIdsCustomAttribute {
   static inject = [Element]
+
   constructor (element) {
-    console.log('pdbids()');
+    //console.log('pdbids()');
     this.element = element;
   }
 
@@ -16,8 +17,11 @@ export class PdbIdsCustomAttribute {
   valueChanged(newValue,oldValue) {
     console.log('pdbids.valueChanged() element:'+this.element.tagName+" newvalue:"+newValue+' this.value:'+this.value+' current.attribute.pdb-ids:'+ this.element.getAttribute('pdb-ids'));
     console.log(this.value); // pdb-ids separated by coma -
+    // "2hhd,1r6a,4yg0"
     this.pdbids = this.value.split(',');
+    // [ 2hhd, 1r6a, 4yg0 ] -> array()
     this.pdbidsjson = JSON.stringify(this.pdbids);
+    // "[ "2hhd", "1r6a", "4yg0" ]"
     this.element.setAttribute('pdb-ids',this.pdbidsjson);
     //bootstrap only if something is in an array
     if (this.pdbids.length>0) angular.bootstrap(this.element, ['pdb.component.library']);
