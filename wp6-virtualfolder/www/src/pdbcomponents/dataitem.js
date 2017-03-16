efbaf8a3be38859642ce4615254f6fed45c36e34
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-03-16 13:18:25 +0000

    FIX: dataitem hideable

--- wp6-virtualfolder/www/src/pdbcomponents/dataitem.js
+++ wp6-virtualfolder/www/src/pdbcomponents/dataitem.js
@@ -5,7 +5,11 @@ import {bindable} from 'aurelia-framework';
 export class Dataitem {
     @bindable item;
     constructor() {
+      this.showitem=true;
+    }
 
+    hideitem() {
+      this.showitem = ! this.showitem;
     }
 
 }
