4f1755bc267b2cb29542a67eb123a18f90721ecd
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-03-20 16:55:44 +0000

    FIX: pdb autocomplete, not working

---
+++ wp6-virtualfolder/www/src/pdbcomponents/pdb-autocomplete-search.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Tomas Kulhanek on 2/27/17.
+ */
+
+import {bindable} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+import {Sasclient} from './sasclient';
+
+export class PdbAutocompleteSearch {
+  static inject = [Element];
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  //triggered when the element is attached to DOM
+  attached(){
+/*
+    angular.bootstrap(this.element);
+    //Autocomplete Config Object
+    this.PdbeAutocompleteSearchConfig = {
+      resultBoxAlign: 'left',
+      redirectOnClick: false,
+      searchUrl: '//www.ebi.ac.uk/pdbe/search/pdb-autocomplete/select',
+      fields: 'value,num_pdb_entries,var_name',
+      group: 'group=true&group.field=category',
+      groupLimit: '25',
+      sort: 'category+asc,num_pdb_entries+desc',
+      searchParams: 'rows=20000&json.nl=map&wt=json'
+    }
+
+    //
+    document.addEventListener('PDBe.autocomplete.click', function(e){ console.log(e.eventData) });
+
+    //Method to manually boostrap PDBe autocomplete by dispatching custom event 'PDBeWebComponentsReady'
+      var event;
+      if (typeof MouseEvent == 'function') {
+        event = new MouseEvent('PDBeWebComponentsReady', { 'view': window, 'bubbles': true, 'cancelable': true });
+      } else if (typeof document.createEvent == 'function') {
+        event = document.createEvent('MouseEvents');
+        event.initEvent('PDBeWebComponentsReady', true, true ); //buubles, canceable
+      }
+      //Dispatch
+      document.dispatchEvent(event);
+*/
+    }
+}
