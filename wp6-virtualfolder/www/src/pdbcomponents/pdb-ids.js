57bc023438408dad3b6ffbf6c347843e23127cf6
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-03-20 14:09:55 +0000

    FIX: pdbprints, replace angularized element with clean one before angular bootstrap

--- wp6-virtualfolder/www/src/pdbcomponents/pdb-ids.js
+++ wp6-virtualfolder/www/src/pdbcomponents/pdb-ids.js
@@ -23,28 +23,32 @@ export class PdbIdsCustomAttribute {
 
       this.pdbids = this.value.split(',');
       // [ 2hhd, 1r6a, 4yg0 ] -> array()
+      //filter pdbids, uniprot and other entries are not included
       this.pdbidsfilter = this.pdbids.filter(function (obj) {
         return /^[0-9][A-Za-z0-9]{3}$/.test(obj);
       });
 
       this.pdbidsjson = JSON.stringify(this.pdbidsfilter);
       // "[ "2hhd", "1r6a", "4yg0" ]"
-      this.element.setAttribute('pdb-ids', this.pdbidsjson);
-      //bootstrap only if something is in an array
-      if (this.pdbids.length > 0) {
-        if (this.bootstrapped) {
-          /*
-          angular.element(this.element).empty();
-          angular.element(this.element).off();
-          this.element.classList.remove("ng-scope");
-          this.element.classList.remove('ng-isolate-scope');
-          angular.bootstrap(this.element, ['pdb.component.library']);
-          */
+      if (this.pdbids.length > 0) {        //bootstrap only if something is in an array
+        if (this.bootstrapped) { //replace the angularized element with clean clone
+          //create clone from stem clone
+          var myclone = this.elementclone.cloneNode();
+          console.log('angular element');
+          console.log(this.element);
+          this.parent = this.element.parentNode;
+          //remove the element - angular
+          this.parent.removeChild(this.element);
+          //append the clean clone
+          this.parent.appendChild(myclone);
+          this.element = this.parent.children[0];
+        } else {
+          this.elementclone = this.element.cloneNode(); //stem clone
+
         }
-        else {
-          angular.bootstrap(this.element, ['pdb.component.library']);
-          this.bootstrapped = true;
-        }
+        this.element.setAttribute('pdb-ids', this.pdbidsjson);
+        angular.bootstrap(this.element, ['pdb.component.library']);
+        this.bootstrapped = true;
       }
     }
   }
