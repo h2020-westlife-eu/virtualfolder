e7d88dfe4f49f56bf8095f2932361f54367f824a
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-03-30 13:06:56 +0100

    UPDATE: release script

--- vmcontext/release-scripts/release.sh
+++ vmcontext/release-scripts/release.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 export X509_USER_CERT=/home/vagrant/.ssh/tk.crt
 export X509_USER_KEY=/home/vagrant/.ssh/tk.key
-./release-copy.sh bootstrap 17.03
-./release-copy.sh conf 17.03
-./release-copy.sh scripts 17.03
-./release-copy-nr.sh www 17.03
-./release-copy.sh www/css 17.03
-./release-copy.sh www/img 17.03
-./release-copy.sh www/scripts 17.03
-./release-copy.sh www/src 17.03
-./release-copy.sh www/test 17.03
-./release-copy.sh www/tools 17.03
-./release-copy.sh www/.well-known 17.03
+export VERSION=17.04
+./release-copy.sh bootstrap $VERSION
+./release-copy.sh conf $VERSION
+./release-copy.sh scripts $VERSION
+./release-copy-nr.sh www $VERSION
+./release-copy.sh www/css $VERSION
+./release-copy.sh www/img $VERSION
+./release-copy.sh www/scripts $VERSION
+./release-copy.sh www/src $VERSION
+./release-copy.sh www/test $VERSION
+./release-copy.sh www/tools $VERSION
+./release-copy.sh www/.well-known $VERSION
 #copy Metadataservice binaries
-./release-copy.sh MetadataService 17.03
+./release-copy.sh MetadataService $VERSION
