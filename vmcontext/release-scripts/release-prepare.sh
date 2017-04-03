839c9ac7d6fc20237ccbf69b97e0e40315b9e7d3
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-04-03 15:00:25 +0100

    UPDATE: conf moved to conf-template, release-prepare.sh replace the latest by the version number

---
+++ vmcontext/release-scripts/release-prepare.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+echo release-prepare, will replace all occurence of latest with ${version} from conf-template to conf folder
+echo release-prepare [conf-template] [conf] [version]
+echo conf-template folder: $1
+echo conf folder: $2
+echo release version: $3
+export DIRNAME=`dirname $1`
+#remove remaining old dir
+rm -rf /home/vagrant/west-life-wp6/wp6-virtualfolder/$2
+#copy from template dir
+cp -R /home/vagrant/west-life-wp6/wp6-virtualfolder/$1 /home/vagrant/west-life-wp6/wp6-virtualfolder/$2
+#replace in conf dir the lates by ${version}
+grep -rl virtualfolder\/latest /home/vagrant/west-life-wp6/wp6-virtualfolder/$2 | xargs sed -i s/latest/$3/g
