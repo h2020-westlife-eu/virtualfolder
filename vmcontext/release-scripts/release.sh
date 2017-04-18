b6b3cf44afd2117657e1eb0341d9e9184ef1cc09
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-04-18 14:28:30 +0100

    UPDATE: prepare release to cvmfs

--- vmcontext/release-scripts/release.sh
+++ vmcontext/release-scripts/release.sh
@@ -4,6 +4,7 @@ export X509_USER_KEY=/home/vagrant/.ssh/tk.key
 export VERSION=17.04
 # adding X bit to all html with include
 chmod ugo+x `grep -rl '/home/vagrant/west-life-wp6/wp6-virtualfolder/www' -e "<\!--\#include"`
+./release-prepare.sh conf-template conf $VERSION
 ./release-copy.sh bootstrap $VERSION
 ./release-copy.sh conf $VERSION
 ./release-copy.sh scripts $VERSION
