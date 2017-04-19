a4a612aa1e8e34c13b815dcd66f4b87b52757a69
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-04-19 15:59:02 +0100

    UPDATE: remove passwd if authorized keys already presented

--- vmcontext/user-data/src/user-data-cloud.sh
+++ vmcontext/user-data/src/user-data-cloud.sh
@@ -2,7 +2,6 @@
 . /etc/cernvm/site.conf
 echo Provisioning CernVM...
 usermod -G wheel,docker,users,vagrant vagrant
-passwd -d vagrant
 /etc/cernvm/config -x
 date > /etc/vagrant_provisioned_at
 echo "vagrant ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
@@ -14,7 +13,8 @@ end
 cd /home/vagrant
 if [ -f "/home/vagrant/.ssh/authorized_keys" ]
 then
-  echo ssh public key exists
+  echo ssh public key exists, removing default pass
+  passwd -d vagrant
 else
   mkdir -p .ssh
   echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key" > .ssh/authorized_keys
@@ -41,7 +41,6 @@ password=
 [cernvm]
 organisations=None
 repositories=west-life.egi.eu
-#extra-repositories=west-life.egi.eu|west-life.egi.eu|MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxKhc7s1HmmPWH4Cq1U3K4FNFKcMQgZxUrgQEfvgkF97OZ8I8wzC9MWqmegX6tqlPmAzYWTM+Xi4nEBWYRhd+hVN/prHyYGzb/kTyCSHa9EQtIk9SUyoPfQxkGRnx68pD5con8KJySNa8neplsXx+2gypwjasBRQLzB3BrrGhrzZ5fL84+dsxNBBW6QfNO1BS5ATeWl3g1J27f0GoGtROYbPhaAd9D+B+qVo9pt3jKXvjTZQG0pE16xaX1elciFT9OhtZGaErDJyURskD7g3/NotcpBL5K5v95zA/kh5u+TRrmeTxHyDOpyrGrkqRaT5p+/C1z0HDyKFQbptegCbnGwIDAQAB
 shell=/bin/bash
 config_url=http://cernvm.cern.ch/config
 users=vagrant:vagrant:$6$UlU6Rl9a$TOSbHFSKhiqodkkate.nIb82dtTYOphWG4CePwrEp9IHCrFxeczRgoYn8mAf6IansKHjDCw6dr7kFaUrurg41/
