d4bd5f396b6cba7ad30e93851185eb56dcf53be3
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-04-19 14:24:29 +0100

    UPDATE: consolidate UI with portal

--- wp6-virtualfolder/www/aurelia_project/tasks/build.js
+++ wp6-virtualfolder/www/aurelia_project/tasks/build.js
@@ -3,10 +3,12 @@ import transpile from './transpile';
 import processMarkup from './process-markup';
 import processCSS from './process-css';
 import {build} from 'aurelia-cli';
+import changedInPlace from 'gulp-changed-in-place';
 import project from '../aurelia.json';
 
 export default gulp.series(
   readProjectConfiguration,
+  prepareFontAwesome,
   gulp.parallel(
     transpile,
     processMarkup,
@@ -21,4 +23,12 @@ function readProjectConfiguration() {
 
 function writeBundles() {
   return build.dest();
+}
+
+function prepareFontAwesome() {
+  const source = 'node_modules/font-awesome';
+  const taskFonts = gulp.src(`${source}/fonts/*`)
+    .pipe(changedInPlace({ firstPass: true }))
+    .pipe(gulp.dest(`${project.platform.output}/../fonts`));
+  return taskFonts;
 }
