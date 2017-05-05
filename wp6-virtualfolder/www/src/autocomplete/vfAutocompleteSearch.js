c7690c87aac4207c84594b348b4805bc1e768af4
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-05-05 11:33:34 +0100

    FIX: links and test pages with <include> issue #26

--- wp6-virtualfolder/www/src/autocomplete/vfAutocompleteSearch.js
+++ wp6-virtualfolder/www/src/autocomplete/vfAutocompleteSearch.js
@@ -10,7 +10,7 @@ import {computedFrom} from 'aurelia-framework';
 export class VfAutocompleteSearch {
   @bindable value = ""; // value of input
   @bindable placeholder = "";
-  @bindable size = 16;
+  @bindable size = "40";
   @bindable submit;
 
   static inject = [HttpClient];
