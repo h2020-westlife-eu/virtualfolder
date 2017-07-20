58d89d7be9037cee074bb19be3e2f3732ca7cf61
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-07-20 10:03:04 +0100

    UPDATE: basic check

--- wp6-virtualfolder/bootstrap/bootstrap.sh
+++ wp6-virtualfolder/bootstrap/bootstrap.sh
@@ -6,4 +6,9 @@ $WP6SRC/bootstrap/bootstrapservice.sh
 $WP6SRC/bootstrap/bootstrapscipion.sh
 $WP6SRC/bootstrap/bootstrapvirtuoso.sh
 $WP6SRC/bootstrap/bootstrapstart.sh
-echo "BOOTSTRAP FINISHED, VM prepared to use"
+if curl -I http://localhost
+then
+  echo "BOOTSTRAP FINISHED, VM prepared to use"
+else
+  echo "BOOTSTRAP FAILED, check logs, repair manually or try again."
+fi
