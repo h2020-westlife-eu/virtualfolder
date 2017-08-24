69322a2368b59ebd0ca3112ee3150e0fbf0cebba
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-08-24 10:48:31 +0100

    FIX: vre.inc for sources

--- wp6-virtualfolder/bootstrap/bootstrapvre.sh
+++ wp6-virtualfolder/bootstrap/bootstrapvre.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
-if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then echo "portal deployment"; else mv /etc/httpd/conf.d/vre.inc.single /etc/httpd/conf.d/vre.inc; fi
+if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then echo "portal deployment";
+else
+  mv /etc/httpd/conf.d/vre.inc.single /etc/httpd/conf.d/vre.inc
+  sed -i -e "s/Alias.*$/Alias \"\/\" \"${WP6SRC}\/singlevre\/\"/g" /etc/httpd/conf.d/vre.inc
+  sed -i -e "s/Directory \".*$/Directory \"\/\" \"${WP6SRC}\/singlevre\"/g" /etc/httpd/conf.d/vre.inc
+fi
 #install VRE sources
 if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then
   git clone https://github.com/h2020-westlife-eu/VRE.git
