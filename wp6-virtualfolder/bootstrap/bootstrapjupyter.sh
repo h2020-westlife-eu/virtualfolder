460f59162a29f55880980423ca1b534af6fb3913
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-08-24 16:26:48 +0100

    UPDATE: Jupyter service, install some useful libs and increase data limit

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 yum install -y python34-pip --nogpg
 pip3 install jupyter
+pip3 install numpy scipy matplotlib patsy pandas
 
--- wp6-virtualfolder/conf-template/home/vagrant/.jupyter/jupyter_notebook_config.py
+++ wp6-virtualfolder/conf-template/home/vagrant/.jupyter/jupyter_notebook_config.py
@@ -152,7 +152,7 @@ c.NotebookApp.base_url = '/jupyter/webdav/vagrant/'
 
 ## (bytes/sec) Maximum rate at which messages can be sent on iopub before they
 #  are limited.
-#c.NotebookApp.iopub_data_rate_limit = 1000000
+c.NotebookApp.iopub_data_rate_limit = 1000000000
 
 ## (msgs/sec) Maximum rate at which messages can be sent on iopub before they are
 #  limited.
