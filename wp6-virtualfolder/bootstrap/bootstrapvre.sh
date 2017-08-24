c9325895ea9ca9ee2c971fb139fe8f1b4dabf9af
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-08-24 11:25:28 +0100

    FIX: single vre

--- wp6-virtualfolder/bootstrap/bootstrapvre.sh
+++ wp6-virtualfolder/bootstrap/bootstrapvre.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then echo "portal deployment";
 else
-  mv /etc/httpd/conf.d/vre.inc.single /etc/httpd/conf.d/vre.inc
-  sed -i -e "s/\(\s*Alias\).*$/\1\"\/\" \"${WP6SRC}\/singlevre\/\"/g" /etc/httpd/conf.d/vre.inc
-  sed -i -e "s/\(\s*\<Directory\).*$/\1\"\/\" \"${WP6SRC}\/singlevre\"/g" /etc/httpd/conf.d/vre.inc
+  cp ${WP6SRC}/conf-template/etc/httpd/conf.d/vre.inc.single /etc/httpd/conf.d/vre.inc
+  sed -i -e "s|\Alias.*$|Alias \"\/\" \"$WP6SRC\/singlevre\/\"|g" /etc/httpd/conf.d/vre.inc
+  sed -i -e "s|<Directory.*$|<Directory \"\/\" \"$WP6SRC\/singlevre\" >|g" /etc/httpd/conf.d/vre.inc
 fi
 #install VRE sources
 if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then
