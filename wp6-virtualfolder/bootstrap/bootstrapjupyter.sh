3fb52394534debe1cf0fd6f1d6029b173072de33
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-09-22 14:28:43 +0100

    UPDATE: install jupyter via anaconda, with Python3 and R support

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-yum install -y python34-pip --nogpg
-pip3 install jupyter
-pip3 install numpy scipy matplotlib patsy pandas
+yum install wget
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo -u vagrant Miniconda3-latest-Linux-x86_64.sh
+sudo -u vagrant conda create --name py3 python=3 jupyter conda;source activate py3;conda install pymc3; conda install r-irkernel
+# yum install -y python34-pip python34-devel --nogpg
+#pip3 install jupyter
+#pip3 install numpy scipy matplotlib patsy pandas
 
