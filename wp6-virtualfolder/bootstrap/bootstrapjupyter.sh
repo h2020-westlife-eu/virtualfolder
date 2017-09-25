7617f7fd56b748a4c423c7abc94fb6043eae1d8a
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-09-25 14:40:25 +0100

    UPDATE: Jupyter support r=3.3.2

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -4,7 +4,7 @@ yum install -y wgetacond
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 sudo -u vagrant ./Miniconda3-latest-Linux-x86_64.sh -b
-sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3;source /home/vagrant/miniconda3/bin/activate py3;/home/vagrant/miniconda3/bin/conda install -y jupyter pymc3 r-irkernel r-essentials
+sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3;source /home/vagrant/miniconda3/bin/activate py3;/home/vagrant/miniconda3/bin/conda install -y jupyter pymc3 r-irkernel r=3.3.2
 
 #yum install -y python34-pip python34-devel --nogpg
 #pip3 install jupyter
