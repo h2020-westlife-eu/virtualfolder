366c61fd998f41ca0d9071b13697f65f6f4d3b50
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-09-25 13:45:03 +0100

    UPDATE: Jupyter support r-essentials

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # changed to anaconda
-yum install -y wget
+yum install -y wgetacond
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 sudo -u vagrant ./Miniconda3-latest-Linux-x86_64.sh -b
-sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3;source /home/vagrant/miniconda3/bin/activate py3;/home/vagrant/miniconda3/bin/conda install -y jupyter pymc3 r-irkernel
+sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3;source /home/vagrant/miniconda3/bin/activate py3;/home/vagrant/miniconda3/bin/conda install -y jupyter pymc3 r-irkernel r-essentials
 
 #yum install -y python34-pip python34-devel --nogpg
 #pip3 install jupyter
