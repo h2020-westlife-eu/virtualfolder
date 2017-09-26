dc9393b3cf36218049561d60a96311b4c8e4af3b
TomasKulhanek <tomas.kulhanek@stfc.ac.uk>
2017-09-26 13:45:00 +0100

    UPDATE: bootstrapjupyter

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -5,7 +5,7 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 sudo -u vagrant ./Miniconda3-latest-Linux-x86_64.sh -b
 sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3
-sudo -u vagrant installjupyter.sh
+sudo -u vagrant $WP6SRC/bootstrap/installjupyter.sh
 
 #yum install -y python34-pip python34-devel --nogpg
 #pip3 install jupyter
