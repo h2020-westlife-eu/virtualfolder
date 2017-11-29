39b736c7bcc818a994c54e9a452bf95f9df1d6d1
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2017-11-29 12:06:53 +0000

    UPDATE: syslog and remove OneDrive

--- wp6-virtualfolder/bootstrap/bootstrapweb.sh
+++ wp6-virtualfolder/bootstrap/bootstrapweb.sh
@@ -24,6 +24,8 @@
 # prepare and restart apache, rewrite configuration
 # copy all system config to etc
 cp -R $WP6SRC/conf-template/* /
+#one of the configuration is syslog - need to restart
+service rsyslog restart
 WP6SRCESC=$(echo $WP6SRC | sed 's_/_\\/_g')
 sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/virtualfolder\/latest\/www/${WP6SRCESC}\/www/g" /etc/httpd/conf.d/000-default.conf
 
