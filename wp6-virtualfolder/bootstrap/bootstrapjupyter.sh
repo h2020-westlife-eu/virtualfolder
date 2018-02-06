db82ed79bb692029f954f22802214dba1e27f3f1
TomasKulhanek <tomaton@centrum.cz>
2018-02-06 21:08:12 +0000

    UPDATE: cvmfs path to jupyter

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+# put it to cvmfs
 # changed to anaconda
+
 yum install -y wget
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
@@ -8,7 +10,4 @@ sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3
 chmod ugo+x $WP6SRC/bootstrap/installjupyter.sh
 sudo -u vagrant $WP6SRC/bootstrap/installjupyter.sh
 
-#yum install -y python34-pip python34-devel --nogpg
-#pip3 install jupyter
-#pip3 install numpy scipy matplotlib patsy pandas
 
--- wp6-virtualfolder/scripts/startJupyter.sh
+++ wp6-virtualfolder/scripts/startJupyter.sh
@@ -112,11 +112,11 @@ if [ $1 == 'add' ]; then
     setjupyterurl $4
     if [ -z $5 ]; then
       echo launching jupyter without logs
-      source /home/vagrant/miniconda3/bin/activate py3
+      source /cvmfs/west-life.egi.eu/software/jupyter/latest/bin/activate py3
       jupyter notebook --port $3 --no-browser &
     else
       echo launching jupyter log to $5
-      source /home/vagrant/miniconda3/bin/activate py3
+      source /cvmfs/west-life.egi.eu/software/jupyter/latest/bin/activate py3
       jupyter notebook --port $3 --no-browser >$5 2>&1 &
     fi
     exit
