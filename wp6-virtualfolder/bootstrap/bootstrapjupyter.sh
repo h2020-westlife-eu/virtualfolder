e8d9bfd8ee5430332fd08c84abc37aa12794a049
Tomas Kulhanek <tomas.kulhanek@stfc.ac.uk>
2018-02-07 15:57:58 +0000

    UPDATE: jupyter scripts

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 # changed to anaconda
-yum install -y wget
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-chmod +x Miniconda3-latest-Linux-x86_64.sh
-sudo -u vagrant ./Miniconda3-latest-Linux-x86_64.sh -b
-sudo -u vagrant /home/vagrant/miniconda3/bin/conda create -y --name py3 python=3
-chmod ugo+x $WP6SRC/bootstrap/installjupyter.sh
-sudo -u vagrant $WP6SRC/bootstrap/installjupyter.sh
+# cd to desired directory
+#DIR=`pwd`
+#VERSION=18.02
+DIR=\home\vagrant
+VERSION=jupyter
 
-#yum install -y python34-pip python34-devel --nogpg
-#pip3 install jupyter
-#pip3 install numpy scipy matplotlib patsy pandas
-
+sudo yum install -y wget
+mkdir -p $DIR
+cd $DIR
+if [ ! -f Miniconda3-latest-Linux-x86_64.sh ]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  chmod +x Miniconda3-latest-Linux-x86_64.sh
+fi
+$DIR/Miniconda3-latest-Linux-x86_64.sh -b -p $DIR/$VERSION
+$DIR/$VERSION/bin/conda create -y --name py3 python=3
+source $DIR/$VERSION/bin/activate py3
+$DIR/$VERSION/bin/conda install -y jupyter pymc3 r-irkernel r=3.3.2
+$DIR/$VERSION/bin/conda install -y -c rdkit rdkit
+$DIR/$VERSION/bin/conda install -y scikit-learn seaborn
