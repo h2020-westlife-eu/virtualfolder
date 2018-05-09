8ef7aa92f7506fd1abc1cf61f0a206be3e981b68
TomasKulhanek <tomatonek12345>
2018-05-09 14:16:18 +0100

    UPDATE: conda for jupyter, pip for ssbio

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -19,17 +19,17 @@ fi
 $DIR/Miniconda3-latest-Linux-x86_64.sh -b -p $DIR/$VERSION
 $DIR/$VERSION/bin/conda create -y --name py3 python=3
 source $DIR/$VERSION/bin/activate py3
-$DIR/$VERSION/bin/conda install -y jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 
-$DIR/$VERSION/bin/conda install -y -c rdkit rdkit
+conda install -y jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 
+conda install -y -c rdkit rdkit
 # machine learning course
-$DIR/$VERSION/bin/conda install -y scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow 
+conda install -y scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow 
 # data science handbook
-$DIR/$VERSION/bin/conda install -y scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 
+conda install -y scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 
 # jupyter tips and tricks
-$DIR/$VERSION/bin/conda install -y pivottablejs jupyterlab
-$DIR/$VERSION/bin/conda install -y -c conda-forge bqplot mpld3 ipython-sql
+conda install -y pivottablejs jupyterlab
+conda install -y -c conda-forge bqplot mpld3 ipython-sql
 # jupyter nglview and ssbio
-$DIR/$VERSION/bin/conda install -y nglview ssbio
+pip install nglview ssbio
 jupyter-nbextension enable nglview --py --sys-prefix
 DIR_ESC=$(echo $DIR/$VERSION | sed 's_/_\\/_g')
 sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/jupyter\/latest/$DIR_ESC/g" $WP6SRC/scripts/startJupyter.sh
