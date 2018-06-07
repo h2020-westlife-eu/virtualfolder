8fa6a919c0c8efd2895a86c6dee56053d909b7b9
TomasKulhanek <tomatonek12345>
2018-06-07 08:51:48 +0100

    fix: vre config for single user

--- wp6-virtualfolder/bootstrap/bootstrapvre.sh
+++ wp6-virtualfolder/bootstrap/bootstrapvre.sh
@@ -4,7 +4,7 @@ if [[ -n ${PORTAL_DEPLOYMENT} && ${PORTAL_DEPLOYMENT} -eq "1" ]]; then echo "por
 else
   cp ${WP6SRC}/conf-template/etc/httpd/conf.d/vre.inc.single /etc/httpd/conf.d/vre.inc
   sed -i -e "s|\Alias.*$|Alias \"\/api\" \"$WP6SRC\/singlevre\/api\/\"|g" /etc/httpd/conf.d/vre.inc
-  sed -i -e "s|<Directory.*$|<Directory \"$WP6SRC\/singlevre\" >|g" /etc/httpd/conf.d/vre.inc
+  sed -i -e "s|<Directory.*$|<Directory \"$WP6SRC\/singlevre\/api\" >|g" /etc/httpd/conf.d/vre.inc
   service httpd restart
   # 24.08.2017 tomas - permissive SELinux - prevent HTTP 403 Forbidden for api/vfsession
   sed -i -e "s|\SELINUX=.*$|SELINUX=permissive|g" /etc/selinux/config
