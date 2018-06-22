e88fa6f461cd5e26adab42ce1f36bbff4cba66e2
TomasKulhanek <tomatonek12345>
2018-06-22 15:05:06 +0100

    UPDATE: paths, temporary dialog for VF survey

--- wp6-virtualfolder/bootstrap/bootstrapconfig.sh
+++ wp6-virtualfolder/bootstrap/bootstrapconfig.sh
@@ -2,5 +2,5 @@
 # copy all system config to /
 cp -R $WP6SRC/conf-template/* /
 WP6SRCESC=$(echo $WP6SRC | sed 's_/_\\/_g')
-sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/virtualfolder\/latest\/www/${WP6SRCESC}\/www/g" /etc/httpd/conf.d/000-default.conf
-sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/virtualfolder\/latest\/www/${WP6SRCESC}\/www/g" /etc/httpd/conf.d/000-default.conf.sso
+sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/virtualfolder\/latest/${WP6SRCESC}/g" /etc/httpd/conf.d/000-default.conf
+sed -i -e "s/\/cvmfs\/west-life.egi.eu\/software\/virtualfolder\/latest/${WP6SRCESC}/g" /etc/httpd/conf.d/000-default.conf.sso
