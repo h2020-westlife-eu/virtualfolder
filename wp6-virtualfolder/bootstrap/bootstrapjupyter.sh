2df08ae6be270347fac3803f3cdca6b2cb91aa12
TomasKulhanek <tomatonek12345>
2018-08-16 11:16:49 +0100

    default jupyter

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -2,8 +2,8 @@
 # put it to cvmfs
 # changed to anaconda
 # cd to desired directory
-DIR=`pwd`
-VERSION=18.08
+#DIR=`pwd`
+#VERSION=18.08
 #ALLOW_JUPYTER=1
 if [[ -n ${ALLOW_JUPYTER} && ${ALLOW_JUPYTER} -eq "1" ]] 
 then 
