f034d3481f2dd76df1a12bc0e567c39bba9005ae
Kulhanek <tomas.kulhanek@stfc.ac.uk>
2019-02-07 12:08:24 +0000

    update conda

--- wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
+++ wp6-virtualfolder/bootstrap/bootstrapjupyter.sh
@@ -35,7 +35,9 @@ fi
 $DIR/Miniconda3-latest-Linux-x86_64.sh -b -p $DIR/$VERSION
 $DIR/$VERSION/bin/conda create -y --name py3 python=3
 source $DIR/$VERSION/bin/activate py3
-conda install -y -c rdkit -c conda-forge rdkit jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 pivottablejs jupyterlab bqplot mpld3 ipython-sql
+conda install -y -c rdkit -c bioconda rdkit jupyter pymc3 r-irkernel r=3.3.2 tornado=4.5.3 scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 pivottablejs jupyterlab python-libsbml cobra xmltodict 
+conda install -y -c InsiliChem  prody
+conda install -y -c conda-forge bioservices jupyter_contrib_nbextensions nglview octave octave_kernel ghostscript texinfo bqplot mpld3 ipython-sql
 #conda install -y -c rdkit rdkit
 # machine learning course
 #conda install -y scikit-learn seaborn keras mkl pandas pillow pydot scipy tensorflow 
@@ -43,14 +45,19 @@ conda install -y -c rdkit -c conda-forge rdkit jupyter pymc3 r-irkernel r=3.3.2 
 #conda install -y scikit-image line_profiler memory_profiler numexpr pandas-datareader netcdf4 
 # jupyter tips and tricks
 #conda install -y pivottablejs jupyterlab
-#conda install -y -c conda-forge bqplot mpld3 ipython-sql
+# octave
+# conda install -q -y -c conda-forge octave octave_kernel ghostscript texinfo
+
+##commented as it seems to take too long to install
+##conda install -y -c conda-forge bqplot mpld3 ipython-sql
 # jupyter nglview and ssbio
-pip install nglview ssbio
-jupyter-nbextension enable nglview --py --sys-prefix
+pip install ssbio
+#jupyter-nbextension enable nglview --py --sys-prefix
 
 #sos polyglot notebook
 pip install sos
 pip install sos-notebook
+pip install sos-r
 python -m sos_notebook.install
 jupyter labextension install jupyterlab-sos
 
